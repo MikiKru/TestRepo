c9ea2d5a20212c22adbe2720f58e38b67aef8d33
CCmentorMK <michal.kruczkowski@codecool.com>
2022-10-02 19:06:22 +0200

    v0

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Thread myThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (this) {
+                    try {
+                        this.wait(10000);
+                        System.out.println("Thread in runnable state");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        myThread.start();
+    }
+}
